10190b11c0af2d6d8882c2d57a0df6f0b8734e37
Mateusz Dudziński <mateuszpd7@gmail.com>
2017-06-09 23:25:06 +0200

    Utility namespace.

---
+++ Utility.cpp
@@ -0,0 +1,34 @@
+/*===============================================================================
+Author: Mateusz Dudziński.
+Email: mateuszpd7@gmail.com
+Full Repository: github.com/Mateuszd6/Algorithmics
+Description: Namespace containing some usefull functions.
+===============================================================================*/
+
+namespace utility
+{
+    // Function used to compare template objects, using only '<' operator:
+    template <typename T>
+	inline bool IsLower(T self, T other) { return self < other; }
+    template <typename T>
+    inline bool IsEqual(T self, T other) { return (!(self < other) && !(other < self)); }
+    template <typename T>
+    inline bool IsGreater(T self, T other) { return (!IsEqual(self, other) && !(self < other)); }
+
+    
+    template <typename T>
+	inline bool IsLower(T self, T other, bool(*_compare)(T, T)) 
+    { 
+        return _compare(self, other); 
+    }
+    template <typename T>
+    inline bool IsEqual(T self, T other, bool(*_compare)(T, T)) 
+    {
+         return (!(_compare(self, other)) && !(_compare(other, self))); 
+    }
+    template <typename T>
+    inline bool IsGreater(T self, T other, bool(*_compare)(T, T))
+    { 
+        return (!IsEqual(self, other, _compare) && !(_compare(self, other))); 
+    }
+}
