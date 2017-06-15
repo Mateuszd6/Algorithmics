62a30c1cd42046e3be569e2f9dfe74d7b0d65f36
Mateusz Dudziński <mateuszpd7@gmail.com>
2017-06-15 21:04:43 +0200

    Get biconnected components, bridges and articulation points in the graph.

--- Graph/Biconnected_components.cpp
+++ Graph/Biconnected_components.cpp
@@ -2,32 +2,45 @@
 Author: Mateusz Dudziński.
 Email: mateuszpd7@gmail.com
 Full Repository: github.com/Mateuszd6/Algorithmics
-Description: // TODO:  Time complexity: O(E+V). Space complexity: O(E+V).
+Description: Find articulation points, bridges and biconnected components in the graph.
+             Time complexity: O(E+V). Space complexity: O(E+V).
 Notes:
-       * // TODO: 
+       * This works only for undirected graph as all problems refere to these type of graphs.
+       * All algorithms use low function calculated by _dfsSetLowFunction method.
+       * Time and space complexity is same for every algorithm.
 ===============================================================================*/
 
 #include <iostream>
 #include <vector>
 #include "Graph.cpp"
+#include <unordered_map> // TODO: Implement myself.
 #include "..\Utility.cpp"
 
 // Assing pre order and low values for every vertex in a connected component of a given graph.
 template <class VertexInfo, class EdgeInfo>
 void _dfsSetLowFunction (Graph<VertexInfo, EdgeInfo> &graph, int current, int from, int &time, int* pre_ord, int* low)
 {
+    // Assing pre oder value and increment time variable;
     pre_ord[current] = time++;
 
+    // Default low value is equal to the pre_order time of the vertex.
     low[current] = pre_ord[current];
+
+    // Iterate current node neighbours...
     for (int i = 0; i < graph[current].edges.size(); ++i)
     {
+        // Ignore DFS parent node.
         if (graph[current].edges[i].to == from)
             continue;
+
+        // If the node is already visited update low value.
         if (pre_ord[graph[current].edges[i].to] != 0)
         {
             low[current] = utility::Min(low[current], pre_ord[graph[current].edges[i].to]);
             continue;
         }
+
+        // If not visited yet, call the function recursively and upadte low value.
         _dfsSetLowFunction (graph, graph[current].edges[i].to, current, time, pre_ord, low);
         low[current] = utility::Min(low[current], low[graph[current].edges[i].to]);
     }
@@ -35,7 +48,7 @@ void _dfsSetLowFunction (Graph<VertexInfo, EdgeInfo> &graph, int current, int fr
 
 // Get all articulation points from the given graph.
 template <class VertexInfo, class EdgeInfo>
-std::vector <int> *ArticulationPoints(Graph<VertexInfo, EdgeInfo> &graph)
+std::vector <int> *GetArticulationPoints(Graph<VertexInfo, EdgeInfo> &graph)
 {
     // The result vertor to store id's of articulation points of the graph.
     std::vector<int> *result = new std::vector<int>(0);
@@ -62,6 +75,8 @@ std::vector <int> *ArticulationPoints(Graph<VertexInfo, EdgeInfo> &graph)
             // Make it a root of the DFS subtree and start DFS here 
             // to assing low function in this connected component.
             is_root[i] = true;
+
+            // Call DFS function on the root.
             _dfsSetLowFunction(graph, i, -1, time, pre_order, low);
             
             // If there are two childs with different low values 
@@ -84,17 +99,14 @@ std::vector <int> *ArticulationPoints(Graph<VertexInfo, EdgeInfo> &graph)
 
         // For every edge that comes from current vertex..
         for (int j = 0; j < graph[i].edges.size(); ++j)
-
             // Every edge will be checked once, in the same way it was explored in the DFS algorithm.
             if (pre_order[graph[i].edges[j].from] < pre_order[graph[i].edges[j].to])
-            {
                 // This is the condition for articulation point. It must be true for at least one child.    
                 if (low[graph[i].edges[j].to] >= pre_order[graph[i].edges[j].from])
                 {
                     result->push_back(i);
                     break;
                 }
-            }
     }
     
     // Delete allocated data structures.
@@ -106,7 +118,121 @@ std::vector <int> *ArticulationPoints(Graph<VertexInfo, EdgeInfo> &graph)
     return result;
 }
 
-//*
+// Get all articulation points from the given graph.
+template <class VertexInfo, class EdgeInfo>
+std::vector <typename Graph<VertexInfo, EdgeInfo> :: Edge> *GetBridges(Graph<VertexInfo, EdgeInfo> &graph)
+{
+    // The result vertor to store id's of articulation points of the graph.
+    std::vector <typename Graph<VertexInfo, EdgeInfo> :: Edge> *result 
+        = new std::vector <typename Graph<VertexInfo, EdgeInfo> :: Edge>(0);
+
+    // Array to store preorder numbrer. 
+    // pre_order == 0 means node has not been visited.
+    int *pre_order = new int[graph.Size()]();
+    
+    // Time to determine when given node was reached by the algorithm.
+    int time = 1;
+
+    // Array to store low number.
+    int *low = new int[graph.Size()];
+    
+    // Assing low function to every vertex in the graph.
+    for (int i = 0; i < graph.Size(); ++i)
+        // If vertex is not explored yet...
+        if (pre_order[i] == 0)
+            // Call DFS function on the root.
+            _dfsSetLowFunction(graph, i, -1, time, pre_order, low);
+
+    // Iterate over every edge in the graph
+    for (int i = 0; i < graph.Size(); ++i)
+        for (int j = 0; j < graph[i].edges.size(); ++j)
+            // First condition makes sure that every edge will be check only once,
+            // second checks if given edge is a bridge, based on low function.
+            if (pre_order[graph[i].edges[j].from] < pre_order[graph[i].edges[j].to] &&
+                low[graph[i].edges[j].to] > pre_order[graph[i].edges[j].from])
+                    // Add an edge to the result vector.
+                    result->push_back(graph[i].edges[j]);
+    
+    // Delete allocated data structures.
+    delete[] pre_order;
+    delete[] low;
+    
+    // Return the result.
+    return result;
+}
+
+// Get all biconnected components of the graph.
+template <class VertexInfo, class EdgeInfo>
+std::vector < std::vector < typename Graph<VertexInfo, EdgeInfo> :: Edge > *> *GetBiconnectedComponents(
+    Graph<VertexInfo, EdgeInfo> &graph)
+{
+    std::vector < std::vector < typename Graph<VertexInfo, EdgeInfo> :: Edge > *> *result = 
+        new std::vector < std::vector < typename Graph<VertexInfo, EdgeInfo> :: Edge > *>(0);
+    
+    // Array to store preorder numbrer. 
+    // pre_order == 0 means node has not been visited.
+    int *pre_order = new int[graph.Size()]();
+    
+    // Time to determine when given node was reached by the algorithm.
+    int time = 1;
+
+    // Array to store low number.
+    int *low = new int[graph.Size()];
+
+    // Assing low function to every vertex in the graph.
+    for (int i = 0; i < graph.Size(); ++i)
+        // If vertex is not explored yet...
+        if (pre_order[i] == 0)
+            // Call DFS function on the root.
+            _dfsSetLowFunction(graph, i, -1, time, pre_order, low);
+
+    // Map to store low value into an index in the result array.
+    std::unordered_map<int, int> *low_to_index = new std::unordered_map<int,int>();
+
+    // Iterate over every edge in the graph
+    for (int i = 0; i < graph.Size(); ++i)
+        for (int j = 0; j < graph[i].edges.size(); ++j)
+            // This condition ensures that evey edge will be checked only once.
+            if (pre_order[graph[i].edges[j].from] < pre_order[graph[i].edges[j].to])
+            {
+                // The edge is a bridge which makes it seperate biconnected component.
+                if (low[graph[i].edges[j].to] > pre_order[graph[i].edges[j].from])
+                {
+                    result->push_back(new std::vector <Graph<VertexInfo, EdgeInfo>::Edge>(0));
+                    result->back()->push_back(graph[i].edges[j]);
+                    continue;
+                }
+                
+                // Otherwise low values of both ends of the edge are same.
+                // Therefore, there is a biconnected component that every edge with this 
+                // value on both ends belonds to.
+                std::unordered_map<int,int>::const_iterator got = low_to_index->find(low[graph[i].edges[j].to]);
+                if (got == low_to_index->end())
+                {
+                    // New biconnected component must be added to the result.
+                    result->push_back (new std::vector <Graph<VertexInfo, EdgeInfo>::Edge>(0));
+                    result->back()->push_back(graph[i].edges[j]);
+                    low_to_index->insert(std::make_pair(low[graph[i].edges[j].to], (int)result->size()-1));
+                }
+                else
+                {
+                    // In this case the biconnected component with edges of this value was created, 
+                    // so map is used to get its index.
+                    result->operator[](got->second)->push_back(graph[i].edges[j]);
+                }
+
+            }
+
+    // Delete allocated data structures.
+    delete low_to_index;
+    delete[] pre_order;
+    delete[] low;
+    
+    // Return the result.
+    return result;
+}
+
+/*
 //============== USAGE EXAMPLE ===================
 
 //Empty struct as edges and vertices need no more additional info.
@@ -135,11 +261,38 @@ int main()
     g.AddEdge(14, 0, Empty());
     g.AddEdge(15, 16, Empty());
     g.AddEdge(17, 15, Empty());
-    
-    std::vector <int> *tmp = ArticulationPoints(g);
-    for (auto i : (* tmp))
-        std::cout << i << "\n";
+    std::vector <int> *articulation_points = GetArticulationPoints(g);
+    std::cout << "Articulation points: ";
+    for (auto i : (* articulation_points))
+        std::cout << i << " ";
+    std::vector < Graph<Empty, Empty> :: Edge> *bridges = GetBridges(g);
+    std::cout <<"\nBridges: ";
+    for (auto i : (* bridges))
+        std::cout << i.from << "-" << i.to << " ";
+    std::vector < std::vector <Graph<Empty, Empty> :: Edge > *> *
+        biconnected_components = GetBiconnectedComponents(g);
+    std::cout << "\nBiconnected components:\n";
+    for (int i = 0; i < biconnected_components->size(); ++i)
+    {
+        std::cout << i+1 << ": ";
+        for (auto j : (* (* biconnected_components)[i]))
+            std::cout << j.from << "-" << j.to << " ";
+        std::cout << "\n";
+    }
+    //Output:
+    //  Articulation points: 0 15 1 2 3 5 8
+    //  Bridges: 0-1 0-14 2-3 5-6 8-9 15-16 15-17
+    //  Biconnected components:
+    //  1: 0-1
+    //  2: 0-14
+    //  3: 1-2 1-13 2-7 7-8 8-10 10-11 10-12 10-13 11-12 12-13
+    //  4: 2-3
+    //  5: 3-4 3-5 4-5
+    //  6: 5-6
+    //  7: 8-9
+    //  8: 15-16
+    //  9: 15-17
 
     return 0;
 }
-//*/
+*/
